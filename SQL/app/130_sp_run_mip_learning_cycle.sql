aba28ba746446b2761fe6db8d63ba723cb11f89a
jebjergk <kenneth.jebjerg@baaderbank.de>
2025-11-28 23:06:18 +0100

    Update 130_sp_run_mip_learning_cycle.sql

--- SQL/app/130_sp_run_mip_learning_cycle.sql
+++ SQL/app/130_sp_run_mip_learning_cycle.sql
@@ -55,67 +55,67 @@ begin
 
     if (v_do_signals) then
         call MIP.APP.SP_GENERATE_MOMENTUM_RECS(
-            P_MIN_RETURN,
-            P_MARKET_TYPE,
-            P_INTERVAL_MINUTES
+            :P_MIN_RETURN,
+            :P_MARKET_TYPE,
+            :P_INTERVAL_MINUTES
         );
     end if;
 
     if (v_do_evaluate) then
         call MIP.APP.SP_EVALUATE_MOMENTUM_OUTCOMES(
-            P_HORIZON_MINUTES,
-            P_HIT_THRESHOLD,
-            P_MISS_THRESHOLD,
-            P_MARKET_TYPE,
-            P_INTERVAL_MINUTES
+            :P_HORIZON_MINUTES,
+            :P_HIT_THRESHOLD,
+            :P_MISS_THRESHOLD,
+            :P_MARKET_TYPE,
+            :P_INTERVAL_MINUTES
         );
     end if;
 
     if (v_do_backtest) then
         call MIP.APP.SP_RUN_BACKTEST(
-            P_HORIZON_MINUTES,
-            P_HIT_THRESHOLD,
-            P_MISS_THRESHOLD,
-            v_from_ts,
-            v_to_ts,
-            P_MARKET_TYPE,
-            P_INTERVAL_MINUTES
+            :P_HORIZON_MINUTES,
+            :P_HIT_THRESHOLD,
+            :P_MISS_THRESHOLD,
+            :v_from_ts,
+            :v_to_ts,
+            :P_MARKET_TYPE,
+            :P_INTERVAL_MINUTES
         );
 
         select max(BACKTEST_RUN_ID)
           into v_backtest_run_id
           from MIP.APP.BACKTEST_RUN
-         where MARKET_TYPE = P_MARKET_TYPE
-           and INTERVAL_MINUTES = P_INTERVAL_MINUTES;
+         where MARKET_TYPE = :P_MARKET_TYPE
+           and INTERVAL_MINUTES = :P_INTERVAL_MINUTES;
     else
         v_backtest_run_id := null;
     end if;
 
     if (v_do_train) then
         call MIP.APP.SP_TRAIN_PATTERNS_FROM_BACKTEST(
-            v_backtest_run_id,
-            P_MARKET_TYPE,
-            P_INTERVAL_MINUTES
+            :v_backtest_run_id,
+            :P_MARKET_TYPE,
+            :P_INTERVAL_MINUTES
         );
     end if;
 
     return object_construct(
-        'market_type',       P_MARKET_TYPE,
-        'interval_minutes',  P_INTERVAL_MINUTES,
-        'horizon_minutes',   P_HORIZON_MINUTES,
-        'from_ts',           v_from_ts,
-        'to_ts',             v_to_ts,
-        'backtest_run_id',   v_backtest_run_id,
-        'did_ingest',        v_do_ingest,
-        'did_signals',       v_do_signals,
-        'did_evaluate',      v_do_evaluate,
-        'did_backtest',      v_do_backtest,
-        'did_train',         v_do_train,
-        'msg_ingest',        v_msg_ingest,
-        'msg_signals',       v_msg_signals,
-        'msg_evaluate',      v_msg_eval,
-        'msg_backtest',      v_msg_backtest,
-        'msg_train',         v_msg_train
+        'market_type',       :P_MARKET_TYPE,
+        'interval_minutes',  :P_INTERVAL_MINUTES,
+        'horizon_minutes',   :P_HORIZON_MINUTES,
+        'from_ts',           :v_from_ts,
+        'to_ts',             :v_to_ts,
+        'backtest_run_id',   :v_backtest_run_id,
+        'did_ingest',        :v_do_ingest,
+        'did_signals',       :v_do_signals,
+        'did_evaluate',      :v_do_evaluate,
+        'did_backtest',      :v_do_backtest,
+        'did_train',         :v_do_train,
+        'msg_ingest',        :v_msg_ingest,
+        'msg_signals',       :v_msg_signals,
+        'msg_evaluate',      :v_msg_eval,
+        'msg_backtest',      :v_msg_backtest,
+        'msg_train',         :v_msg_train
     );
 end;
 $$;
